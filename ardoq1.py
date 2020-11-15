6e67b2d5d913fcf89388bc693841deab66531aa1
oscroos <74260894+oscroos@users.noreply.github.com>
2020-11-15 18:09:17 +0100

    Add files via upload

---
+++ ardoq1.py
@@ -0,0 +1,10 @@
+def highest_product(x):
+    c = x.copy() # For å unngå å endre listen 
+    res = 1
+    n = min(3, len(x)) # Antar her at dersom listen har mindre enn 3 elementer så tar vi produktet av alle elementene, og dersom listen er tom returnerer vi 1
+    for _ in range(n):
+        m = max(c)
+        res *= m
+        c.remove(m)
+    return res
+
